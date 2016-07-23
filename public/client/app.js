bddb9a918bf3bcd546919d6e2f2d8c24347c919b
Avikam <avikam@MacBook-Pro.local>
2016-07-23 16:30:59 -0700

    Added counter for each nonsense words rating.

--- public/client/app.js
+++ public/client/app.js
@@ -3,6 +3,12 @@ var server = 'http://127.0.0.1:5000/';
 
 var app;
 
+var numberOfAutomaticCorrect = 0;
+var numberOfLaboredCorrect = 0;
+var numberOfLaboredWrong = 0;
+var numberOfAutomaticWrong = 0;
+var numberOfDidntTry = 0
+
 $(function() {
   app = {
     server: server,
@@ -27,9 +33,58 @@ $(function() {
 
 $('#nonsenseScoring').hide();
 
-$('#nextButton').click(function() {
+$('.nextButton').click(function() {
   console.log('Next button clicked');
   $('#nonsenseScoring').show();
   $('#flashcard').hide();
   app.getData();
 });
+
+$('.button1').click(function() {
+  numberOfAutomaticCorrect++;
+  console.log("Num of Auto Correct " + numberOfAutomaticCorrect);
+  $('#nonsenseScoring').hide();
+  $('#flashcard').show();
+})
+
+$('.button2').click(function() {
+  numberOfLaboredCorrect++;
+  console.log("Num of Labored Correct " + numberOfLaboredCorrect);
+  $('#nonsenseScoring').hide();
+  $('#flashcard').show();
+})
+
+$('.button3').click(function() {
+  numberOfLaboredWrong++;
+  console.log("Num of Labored Wrong " + numberOfLaboredWrong);
+  $('#nonsenseScoring').hide();
+  $('#flashcard').show();
+})
+
+$('.button4').click(function() {
+  numberOfAutomaticWrong++;
+  console.log("Num of Auto Wrong " + numberOfAutomaticWrong);
+  $('#nonsenseScoring').hide();
+  $('#flashcard').show();
+})
+
+$('.button5').click(function() {
+  numberOfDidntTry++;
+  console.log("Num of Didnt Try " + numberOfDidntTry);
+  $('#nonsenseScoring').hide();
+  $('#flashcard').show();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
