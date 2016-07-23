cbedebeb3a57ea754cf68172d412b736546b8c3f
Karen Rose <karen.rose807@gmail.com>
2016-07-23 15:01:58 -0700

    Added buttons and styling

--- public/client/app.js
+++ public/client/app.js
@@ -1,17 +1,6 @@
 var server = 'http://127.0.0.1:5000/';
 // var server = 'https://right2read-9ebcd.firebaseapp.com/';
 
-// Firebase config
-var config = {
-  apiKey: "AIzaSyCtXDxTtW58S-Os2uSizY35nf1ajABm6eU",
-  authDomain: "right2read-9ebcd.firebaseapp.com",
-  databaseURL: "https://right2read-9ebcd.firebaseio.com",
-  storageBucket: "right2read-9ebcd.appspot.com"
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
 var app;
 
 $(function() {
@@ -19,6 +8,7 @@ $(function() {
     server: server,
 
     getData: function() {
+      console.log('Client GET request');
       $.ajax({
         url: app.server,
         type: 'GET',
@@ -35,9 +25,11 @@ $(function() {
   }
 }());
 
+$('#nonsenseScoring').hide();
+
 $('#nextButton').click(function() {
   console.log('Next button clicked');
+  $('#nonsenseScoring').show();
+  $('#flashcard').hide();
   app.getData();
 });
-
-
