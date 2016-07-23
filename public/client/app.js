ff15a1040e3fade17faf9dc7b46b7e82b1e1f42f
Karen Rose <karen.rose807@gmail.com>
2016-07-23 16:41:40 -0700

    Moved Firebase to client

--- public/client/app.js
+++ public/client/app.js
@@ -1,29 +1,40 @@
 var server = 'http://127.0.0.1:5000/';
 // var server = 'https://right2read-9ebcd.firebaseapp.com/';
 
-var app;
+// Firebase config
+var config = {
+  apiKey: "AIzaSyCtXDxTtW58S-Os2uSizY35nf1ajABm6eU",
+  authDomain: "right2read-9ebcd.firebaseapp.com",
+  databaseURL: "https://right2read-9ebcd.firebaseio.com",
+  storageBucket: "right2read-9ebcd.appspot.com"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
-$(function() {
-  app = {
-    server: server,
+// Sounding Out Data
+var soundingOutData, soundSymbol, nonsenseWords;
 
-    getData: function() {
-      console.log('Client GET request');
-      $.ajax({
-        url: app.server,
-        type: 'GET',
-        contentType: 'application/json',
-        success: function(data) {
-          console.log('GET successful');
-          console.log('data', data)
-        },
-        error: function(data) {
-          console.error('GET error');
-        }
-      });
-    }
+database.ref('/sounding-out/').once('value').then(function(snapshot) {
+  soundingOutData = snapshot.val();
+  console.log('sounding out data: ', soundingOutData);
+  // res.status(200).json(soundingOut);
+  soundSymbol = soundingOutData['sound-symbol'];
+  nonsenseWords = soundingOutData['nonsense-words'];
+
+
+  for (var i = 0; i < nonsenseWords.length; i++) {
+    $('#flashcard').prepend('<h1>' + nonsenseWords[0] + '</h1>');
+
+
   }
-}());
+
+
+
+
+
+});
+
+
 
 $('#nonsenseScoring').hide();
 
@@ -31,5 +42,29 @@ $('#nextButton').click(function() {
   console.log('Next button clicked');
   $('#nonsenseScoring').show();
   $('#flashcard').hide();
-  app.getData();
 });
+
+
+// var app;
+
+// $(function() {
+//   app = {
+//     server: server,
+
+//     getData: function() {
+//       console.log('Client GET request');
+//       $.ajax({
+//         url: app.server,
+//         type: 'GET',
+//         contentType: 'application/json',
+//         success: function(data) {
+//           console.log('GET successful');
+//           console.log('data', data)
+//         },
+//         error: function(data) {
+//           console.error('GET error');
+//         }
+//       });
+//     }
+//   }
+// }());
--- public/server/server.js
+++ public/server/server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var server = express();
+// var firebase = require('firebase');
 
-var defaultHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10, // Seconds.
-  'Content-Type': "application/json"
-};
+// var defaultHeaders = {
+//   "access-control-allow-origin": "*",
+//   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+//   "access-control-allow-headers": "content-type, accept",
+//   "access-control-max-age": 10, // Seconds.
+//   'Content-Type': "application/json"
+// };
 
 server.options('/', function(req, res) {
   console.log('options successful');
@@ -15,13 +16,13 @@ server.options('/', function(req, res) {
 });
 
 server.get('/', function (req, res) {
-  var responseData = {
-    'text' : 'Hello Right2Read',
-    'status' : 'successful'
-  };
+  console.log('GET REQUEST');
 
-  console.log('response data', responseData);
-  res.status(200).json(responseData);
+  database.ref('/sounding-out/').once('value').then(function(snapshot) {
+    soundingOut = snapshot.val();
+    console.log('sounding out: ', soundingOut);
+    res.status(200).json(soundingOut);
+  });
 });
 
 server.listen(5000, function () {
