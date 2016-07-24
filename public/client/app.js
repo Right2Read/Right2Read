addf2fbd745224f73f1a2805e4e4a45ad1737f01
Karen Rose <karen.rose807@gmail.com>
2016-07-24 02:00:05 -0700

    Added separate student and instructor views to be viewed on separate devices

--- public/client/app.js
+++ public/client/app.js
@@ -1,42 +1,46 @@
-// var server = 'http://127.0.0.1:5000/';
-var server = 'https://right2read-9ebcd.firebaseapp.com/';
-
-// Firebase config
-var config = {
-  apiKey: "AIzaSyCtXDxTtW58S-Os2uSizY35nf1ajABm6eU",
-  authDomain: "right2read-9ebcd.firebaseapp.com",
-  databaseURL: "https://right2read-9ebcd.firebaseio.com",
-  storageBucket: "right2read-9ebcd.appspot.com"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
-
-// Sounding Out Data
-var soundingOutData, soundSymbol, nonsenseWords;
-
-database.ref('/sounding-out/').once('value').then(function(snapshot) {
-  soundingOutData = snapshot.val();
-  console.log('sounding out data: ', soundingOutData);
-  // res.status(200).json(soundingOut);
-  soundSymbol = soundingOutData['sound-symbol'];
-  nonsenseWords = soundingOutData['nonsense-words'];
-
+$(document).ready(function() {
+  // var server = 'http://127.0.0.1:5000/';
+  var server = 'https://right2read-9ebcd.firebaseapp.com/';
+  var type;
   var wordCounter = 0;
-  showFlashcard();
 
-  function showFlashcard() {
-    $('#nonsenseScoring').hide();
+  // Firebase config
+  var config = {
+    apiKey: "AIzaSyCtXDxTtW58S-Os2uSizY35nf1ajABm6eU",
+    authDomain: "right2read-9ebcd.firebaseapp.com",
+    databaseURL: "https://right2read-9ebcd.firebaseio.com",
+    storageBucket: "right2read-9ebcd.appspot.com"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
 
-    if (wordCounter < nonsenseWords.length) {
-      $('#flashcard').prepend('<h1>' + nonsenseWords[wordCounter] + '</h1>');
-      $('#question').show();
-      wordCounter++;
-    } else {
-      showSoundingOutSummary();
-      showRecommendations();
-    }
+  // Sounding Out Data
+  var soundingOutData, soundSymbol, nonsenseWords;
 
-  }
+  database.ref('/sounding-out/').once('value').then(function(snapshot) {
+    soundingOutData = snapshot.val();
+    console.log('sounding out data: ', soundingOutData);
+    soundSymbol = soundingOutData['sound-symbol'];
+    nonsenseWords = soundingOutData['nonsense-words'];
+  });
+
+  $('#studentButton').click(function() {
+    console.log('student button clicked');
+    $('#studentButton').hide();
+    $('#instructorButton').hide();
+    $('#authButton').hide();
+    type = 'student';
+    showFlashcard();
+  });
+
+  $('#instructorButton').click(function() {
+    console.log('instructor button clicked');
+    $('#studentButton').hide();
+    $('#instructorButton').hide();
+    $('#authButton').hide();
+    showTeacherRating();
+    type = 'instructor';
+  });
 
   function showRecommendations() {
     $('#videoRecommendations').show();
@@ -45,155 +49,211 @@ database.ref('/sounding-out/').once('value').then(function(snapshot) {
     $('#programRecommendations').show();
   }
 
-  $('.nextButton').click(function() {
-    console.log('Next button clicked');
-    showTeacherRating();
-    $('#flashcard h1').remove();
-  });
+    // $('.nextButton').click(function() {
+    //   console.log('Next button clicked');
+    //   showTeacherRating();
+    //   $('#flashcard h1').remove();
+    // });
 
   function showTeacherRating() {
-    $('#question').hide();
+    // $('#question').hide();
     $('#nonsenseScoring').show();
   }
 
+  //toggle value so the word on the student screen shows up
+  function toggleValue() {
+    var update = {
+      'scoringComplete' : Math.random()
+    }
+    firebase.database().ref().update(update);
+  }
+
+  function wordsComplete() {
+    var update = {
+      'wordsComplete' : Math.random()
+    }
+    firebase.database().ref().update(update);
+  }
+
   $('.button1').click(function() {
+    toggleValue();
     numberOfAutomaticCorrect++;
     console.log("Num of Auto Correct " + numberOfAutomaticCorrect);
-    showFlashcard();
-  })
+    // showFlashcard();
+  });
 
   $('.button2').click(function() {
+    toggleValue();
     numberOfLaboredCorrect++;
     console.log("Num of Labored Correct " + numberOfLaboredCorrect);
-    showFlashcard();
-  })
+    // showFlashcard();
+  });
 
   $('.button3').click(function() {
+    toggleValue();
     numberOfLaboredWrong++;
     console.log("Num of Labored Wrong " + numberOfLaboredWrong);
-    showFlashcard();
-  })
+    // showFlashcard();
+  });
 
   $('.button4').click(function() {
+    toggleValue();
     numberOfAutomaticWrong++;
     console.log("Num of Auto Wrong " + numberOfAutomaticWrong);
-    showFlashcard();
-  })
+    // showFlashcard();
+  });
 
   $('.button5').click(function() {
+    toggleValue();
     numberOfDidntTry++;
     console.log("Num of Didn't Try " + numberOfDidntTry);
+    // showFlashcard();
+  });
+
+
+  var app;
+
+  var numberOfAutomaticCorrect = 0;
+  var numberOfLaboredCorrect = 0;
+  var numberOfLaboredWrong = 0;
+  var numberOfAutomaticWrong = 0;
+  var numberOfDidntTry = 0
+
+  function showSoundingOutSummary() {
+    console.log('Summary');
+    var range = [
+      [0, 5],
+      [6, 8],
+      [9, 15]
+    ];
+    var score = ['Weak', 'Ok', 'Strong'];
+    var result = numberOfAutomaticCorrect + Math.floor(numberOfLaboredCorrect * .5);
+
+    for (var i = 0; i < range.length; i++) {
+      var lowEnd = range[i][0];
+      var highEnd = range[i][1];
+
+      if (result > lowEnd && result < highEnd) {
+        console.log(score[i], result);
+      }
+    }
+
+    $('#summary').show();
+  }
+
+  $('#authButton').text('Login');
+
+  // $('#authButton').click(function() {
+  //   $(this.text)('Login');
+  // };
+
+  $('#authButton').click(function() {
+    console.log('Button value is ', $(this).val());
+
+    if ($(this).val() === 'Logout') {
+      console.log('Logging out');
+      logout();
+    } else {
+      console.log('Login clicked');
+      login();
+    }
+  });
+
+  function login() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+
+      $('#authButton').text('Logout');
+
+      console.log('logged in');
+
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+
+      console.log(errorCode, errorMessage, email, credential);
+    });
+  }
+
+  function logout() {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      console.log('Signout successful');
+    }, function(error) {
+      // An error happened.
+      console.log(error);
+    });
+  }
+
+  firebase.database().ref('/scoringComplete/').on('value', function(snapshot) {
+    //updateStarCount(postElement, snapshot.val());
+    console.log('Scoring state changed ', snapshot.val());
     showFlashcard();
   });
-});
 
-var app;
+  firebase.database().ref('/wordsComplete/').on('value', function(snapshot) {
+    //updateStarCount(postElement, snapshot.val());
+    console.log('Words complete state changed ', snapshot.val());
 
-var numberOfAutomaticCorrect = 0;
-var numberOfLaboredCorrect = 0;
-var numberOfLaboredWrong = 0;
-var numberOfAutomaticWrong = 0;
-var numberOfDidntTry = 0
-
-function showSoundingOutSummary() {
-  console.log('Summary');
-  var range = [
-    [0, 5],
-    [6, 8],
-    [9, 15]
-  ];
-  var score = ['Weak', 'Ok', 'Strong'];
-  var result = numberOfAutomaticCorrect + Math.floor(numberOfLaboredCorrect * .5);
-
-  for (var i = 0; i < range.length; i++) {
-    var lowEnd = range[i][0];
-    var highEnd = range[i][1];
-
-    if (result > lowEnd && result < highEnd) {
-      console.log(score[i], result);
+    if (type === 'instructor') {
+      $('#nonsenseScoring').hide();
+      showSoundingOutSummary();
+      showRecommendations();
     }
+  });
+
+  function showFlashcard() {
+    // $('#nonsenseScoring').hide();
+    if (wordCounter < nonsenseWords.length) {
+      if (type === 'student') {
+        $('#flashcard h1').remove();
+        $('#flashcard').prepend('<h1>' + nonsenseWords[wordCounter] + '</h1>');
+        $('#question').show();
+        wordCounter++;
+      }
+    } else {
+      wordsComplete();
+    }
+
+    console.log("type", type, 'word counter', wordCounter);
   }
 
-  $('#summary').show();
-}
 
-$('#authButton').text('Login');
 
-// $('#authButton').click(function() {
-//   $(this.text)('Login');
-// };
 
-$('#authButton').click(function() {
-  console.log('Button value is ', $(this).val());
 
-  if ($(this).val() === 'Logout') {
-    console.log('Logging out');
-    logout();
-  } else {
-    console.log('Login clicked');
-    login();
-  }
+  // var app;
+
+  // $(function() {
+  //   app = {
+  //     server: server,
+
+  //     getData: function() {
+  //       console.log('Client GET request');
+  //       $.ajax({
+  //         url: app.server,
+  //         type: 'GET',
+  //         contentType: 'application/json',
+  //         success: function(data) {
+  //           console.log('GET successful');
+  //           console.log('data', data)
+  //         },
+  //         error: function(data) {
+  //           console.error('GET error');
+  //         }
+  //       });
+  //     }
+  //   }
+  // }());
 });
-
-function login() {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  provider.addScope('https://www.googleapis.com/auth/plus.login');
-
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-
-    $('#authButton').text('Logout');
-
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-
-    console.log(errorCode, errorMessage, email, credential);
-  });
-}
-
-function logout() {
-  firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-    console.log('Signout successful');
-  }, function(error) {
-    // An error happened.
-    console.log(error);
-  });
-}
-
-
-
-
-
-// var app;
-
-// $(function() {
-//   app = {
-//     server: server,
-
-//     getData: function() {
-//       console.log('Client GET request');
-//       $.ajax({
-//         url: app.server,
-//         type: 'GET',
-//         contentType: 'application/json',
-//         success: function(data) {
-//           console.log('GET successful');
-//           console.log('data', data)
-//         },
-//         error: function(data) {
-//           console.error('GET error');
-//         }
-//       });
-//     }
-//   }
-// }());
