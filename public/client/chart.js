eba3daa28fce2d400114db34a8967b597497c6c3
Karen Rose <karen.rose807@gmail.com>
2016-07-23 23:52:13 -0700

    Cleaned up summary page and chart

--- public/client/chart.js
+++ public/client/chart.js
@@ -1,4 +1,6 @@
 var ctx = document.getElementById("myChart");
+Chart.defaults.global.defaultFontSize = 20;
+Chart.defaults.global.legend.display = false;
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -7,18 +9,18 @@ var myChart = new Chart(ctx, {
             // label: 'Responses at a Glance',
             data: [2, 3.5, 5, 3.5, 2.9],
             backgroundColor: [
+                '#00b8e6',
                 '#4CAF50',
-                'rgb(225,153,0)',
-                'rgb(225,153,0)',
-                '#f44336',
-                '#f44336'
+                '#0033cc',
+                '#cc00cc',
+                '#00cc99',
             ],
             borderColor: [
-               '#4CAF50',
-                'rgb(225,153,0)',
-                'rgb(225,153,0)',
-                '#f44336',
-                '#f44336'
+                '#00b8e6',
+                '#4CAF50',
+                '#0033cc',
+                '#cc00cc',
+                '#00cc99',
             ],
             borderWidth: 1
             }]
